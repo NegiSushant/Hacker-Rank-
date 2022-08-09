dfa06647fb69e930418476d9c65f46e0a0c27c68
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-08-09 21:47:16 +0530

    Add files via upload

---
+++ 02_Python Problems/Python Division.py
@@ -0,0 +1,14 @@
+#Task-->>>>
+#          The provided code stub reads two integers, a and b, from STDIN.
+
+# Add logic to print two lines. The first line should contain the result of integer division, a // b. The second line should contain the result of float division,  a/b .
+
+# No rounding or formatting is necessary.
+
+a = int(input("Please enter the number: "))
+b = int(input("Please enter the second number: "))
+Int_D = a//b
+f_d = a/b
+    
+print(Int_D)
+print(f_d)
