08583e387f496eff384edbc789313aa35e5a7d80
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-08-09 21:33:46 +0530

    Add files via upload

---
+++ 02_Python Problems/ArithmeticOperators.py
@@ -0,0 +1,16 @@
+# Task
+# The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+a = int(input("Please enter the first number: "))
+b = int(input("Please enter the Second number: "))
+
+sum = a + b
+diff = a - b
+pro = a * b
+
+print(sum)
+print(diff)
+print(pro)
