43bd6424a02cf5bb480eacf8b3fca6ffcca935f6
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-08-10 21:24:42 +0530

    Add files via upload

---
+++ 02_Python Problems/PrintFunction.py
@@ -0,0 +1,6 @@
+#https://www.hackerrank.com/challenges/python-print/problem
+#Task--->>>
+#Print the list of integers from  through  as a string, without spaces.
+n = int(input("Please enter the number: "))
+for i in range(1, n+1):
+  print(i,end='')
